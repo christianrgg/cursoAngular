9f9cccab3b73e536efdcff64e4c4d09595b85049
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-01 18:35:08 -0600

    TypeScript: Funciones basicas

---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,31 @@
+
+//Los argumentos de las funciones deben llevar el tipo de datos que aceptaran, en caso de no saber cual agregar colocar any ejemplo a:any. 
+//Si se desea que el tipado de TypeScript sea muy estricto cambiar la configuración del archivo tsconfig.json
+function addNumbers (a:number, b:number){
+    return a + b;
+}
+const result: number = addNumbers(1,2);
+console.log({result});
+
+//FUNCIONES DE FLECHA =>
+// Se puede colocar el tipo de dato que la función regresa agregandolo despues de los argumentos.
+const addNumbersArrow = (a:number, b:number):string => {
+    //Dentro de las comillas inversas se puede hacer la función toString();
+    return `${a+b}`;
+}
+const result2: String = addNumbersArrow(1,2);
+console.log({result2});
+
+
+//En las siguiente función el primer caracter es obligatorio, el segundo por signo de interrogación se hace opcional y el tercer valo se hace opcional pero con valor por defecto. Se aconseja seguir ese orden. 
+function multiply(firstNumber:number, secondNumber?:number, base:number=2){
+    return firstNumber * base;
+}
+
+const multiplyResult: number = multiply(5);
+console.log({multiplyResult});
+
+
+export {
+
+}
