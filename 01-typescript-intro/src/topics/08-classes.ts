6be2baf88b084f566e16ca6fd219d622fb9efb31
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-05 10:05:55 -0600

    TypeScript: Constructor de una clase

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,12 +1,30 @@
 export class Persona {
-    public name?: string;
-    public address?: string;
+    public name: string;
+    public address: string;
 
-    constructor(){
-        this.name = "Christian";
-        this.address = "Mérida"
+    constructor(name:string, address:string ){
+        this.name = name;
+        this.address = address;
     }
 }
 
-const ironman = new Persona();
+//Ejemplo de clase y constructor resumido
+
+export class Player {
+    constructor(
+        public name: string,
+        public team: string,
+        public score: number,
+    ){
+        this.name = name;
+        this.team = team;
+        this.score = score
+    }
+}
+
+
+const ironman = new Persona("Iron Man", "New York");
  console.log(ironman);
+
+ const messi = new Player ("Messi", "Paris", 98)
+ console.log(messi);
