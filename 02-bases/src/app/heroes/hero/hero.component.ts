3c4ebe8685388133a708edb5228f6c706421d54c
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-07 10:04:12 -0600

    Angular: Enlazado en una sola via

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -10,5 +10,13 @@ export class HeroComponent {
   public name: string = "ironman";
   public age:  number = 45;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  // Si se le coloca private el metodo no se puede usar, solo dentro de la clase y el html esta fuera de la clase
+  public getHeroesDescription():string {
+    return `${this.name} - ${this.age}`
+  }
 
 }
