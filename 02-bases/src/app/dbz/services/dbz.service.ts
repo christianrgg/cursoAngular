18b84469fe48cb732ab38a34f2a3b802ca41864a
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-08 21:32:58 -0600

    Angular: Paquetes externos - UUID

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,31 +1,40 @@
-//Para generar el servicio es a-service
-//Para generar las importaciones faltantes posicionarse sobre el objeto, interface etc y presiona CTRL + .
-//Cambiar el nombre de el servicio
-//Colocar los metodos necesarios
+//Pasos para colocar ID de dependencias externas
+//1.Ver https://www.npmjs.com/package/uuid e instalar "npm i uuid"
+//2. Instalar la siguiente dependencia "npm i --save-dev @types/uuid"
+//3. Importar y asignar como funcion uuid "import { v4 as uuid } from 'uuid';"
+//4. Agregar id en character y en valor llamar la funcion "id: uuid(),"
+//5. En metodo newCharacter, crear un nuevo objeto y pasarle la propiedad id.
+//6. Que ese metodo agregue/empuje al listado el new Character
 
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({providedIn: 'root'})
 export class DbzService {
 
   public characters: Character[] = [
     {
+      id: uuid(),
       name: 'Krilin',
       power: 1000
     },
     {
+      id: uuid(),
       name: 'Goku',
       power: 9500
     },
     {
+      id: uuid(),
       name: 'Vegueta',
       power: 9000,
     }
   ];
 
   onNewCharacter( character: Character):void{
-    this.characters.push(character);
+    const newCharacter: Character = {id: uuid(), ...character}
+
+    this.characters.push(newCharacter);
   }
 
   onDeleteCharacter(index:number){
