cc258ecc4096745c96497decefc32b173ce22ace
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-11 12:12:25 -0600

    GifsApp: Pagina home: componente hijo card-list

---
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,11 @@
+// 23. Generar el componente hijo card list "ng g component gifs/components/card-list"
+//24. Eliminar los archivos spec y css, renombrar el selectror "gifs-card-list"
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html',
+})
+export class CardListComponent {
+
+}
--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 // 16. Exportar el compornente HomePageComponent
@@ -9,7 +10,8 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
