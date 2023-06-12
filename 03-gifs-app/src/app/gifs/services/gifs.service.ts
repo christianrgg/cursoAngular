70c8bdd430ddf09c2a3f87d47b5dde9b315407be
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-11 20:46:39 -0600

    GifsApp: Giphy api key

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -4,31 +4,33 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagHistory: string[] = [];
-
+// 54. Crear propiedad con clave de Apikey
+  private apiKey: string = '4wx7dk58fbdCKoFJmnbzMlea2WXb9wvd'
+// 55. En postman Crear una nueva app, ir a la documentación y copiar la gifURL y pegarla en postaman "api.giphy.com/v1/gifs/search?api_key=4wx7dk58fbdCKoFJmnbzMlea2WXb9wvd" cambiar la ultima palabra por search, agregar apikey y clic en enviar
+// 56. En los query parametros agregar q - valoran y limit - 10
   get tagHistory(){
     return [...this._tagHistory]
   }
 
-// 49. Crear un metodo privado para organizar los tags de modo que no coloque los que se repitan
+
   private organizeHistory(tag:string):void{
-// 50. Convertir las etiquetas en minusculas porque javascript es lowercase sensitive
+
     tag = tag.toLowerCase();
 
-// 51. Condicion para revisar si etiqueta ya existe en el arregle, si existe eliminarla y pasar la nueva etiqueta igual al inicio del arreglo.
+
     if(this._tagHistory.includes(tag)){
       this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !==tag)
     }
     this._tagHistory.unshift(tag);
-// 52. Condicion para limitar la longitud del arreglo a 10 elementos
+
     this._tagHistory = this._tagHistory.slice(0,10);
   }
 
 
   public searchTag (tag:string):void {
-// 48. Agregar validacion para caso que el tag este vacio. Considera que es string
+
     if(tag.length === 0) return;
 
-// 53. Llamar al metodo organizar historial
     this.organizeHistory(tag);
   }
 
