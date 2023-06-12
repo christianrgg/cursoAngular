d67d6a8dfb4ddf81a577d24dbc394fa976089cda
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-12 01:47:18 -0600

    GifsApp: Colocar tipo de dato a a la respuesta HTTP

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
+// 66. Crear constante que almacene los gifs, verificar la importacion de la interface e inicializarla vacia.
+  public gifList: Gif[] = [];
   private _tagHistory: string[] = [];
   private apiKey: string = '4wx7dk58fbdCKoFJmnbzMlea2WXb9wvd'
-  //60. Optimización del url
   private serviceUrl:string = 'https://api.giphy.com/v1/gifs'
 
-  // 58. Inyectar el modulo httpclientemodule
   constructor(private http: HttpClient) {}
 
   get tagHistory(){
@@ -35,16 +36,17 @@ export class GifsService {
 
     if(tag.length === 0) return;
     this.organizeHistory(tag);
-    // 61. Para optimizar la url con los parametros, agregarlos a una clase HttpParams, asegurarse de importarlo
+
     const params = new HttpParams()
       .set('api_key', this.apiKey)
       .set('limit', 10)
       .set('q', tag)
-
-    // 59. Copiar la liga generada por postman agregar https:// y pegarla en el llamado del servicio http (OBSERVABLE) y suscribirse para escuchar los llamados la primera parte del url va en el paso 60 "https://api.giphy.com/v1/gifs/search?api_key=4wx7dk58fbdCKoFJmnbzMlea2WXb9wvd&q=valorant&limit=10"
-    this.http.get(`${this.serviceUrl}/search`,{params})
+// 65. Indicarle a la petición que tipo dato es, para este caso es searchresponse
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
       .subscribe( resp =>{
-        console.log(resp);
+//67. Asignar respuesta con gif a variable que los almacena
+        this.gifList = resp.data;
+        console.log({gifs: this.gifList});
       })
 
   }
