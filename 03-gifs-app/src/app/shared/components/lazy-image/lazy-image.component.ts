34ff0fde0a163fc9820f605208577d715a4951d8
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-13 19:16:23 -0600

    GifsApp: Estilos CSS de las tarjetas

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,32 +1,28 @@
 import { OnInit } from '@angular/core';
-// 91. Generar el componente Lazy-Image (Es este archivo).
 import { Component, Input } from '@angular/core';
-// 92. Cambiar el nombre del selector por "shared-lazy-image"
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-// 97. Agregar validador OnIniti y actualizar la interfaz a lazy...
 export class LazyImageComponent implements OnInit {
-  // 96. Agregar escuchador de eventos para el padre o decorador input y agregar el non null operator
   @Input()
   public url!:string
 
-  //100. Agregar input para el alt del html del paso 99
   @Input()
   public alt:string = '';
 
-// 105 Crear propiedad publica para saber si ya cargo o no ha cargado la imagen.
+
   public hasLoaded: boolean = false;
 
   ngOnInit(): void {
-//98. Agregar validaciones
+
     if(!this.url) throw new Error('Url property is required.');
   }
 
-//106. Crear un metodo para decir si la imagen esta cargada...
+// 112. Agregar tiempo del spinner
   onLoad(){
-    console.log('Image Loaded');
-    this.hasLoaded = true;
+    setTimeout(()=>{
+      this.hasLoaded = true;
+    },1000);
   }
 }
