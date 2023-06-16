c413367ee6fcf9299b0d22a1f4576a7b09667022
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-16 15:01:09 -0600

    CountryApp: Debounce - Peticiones cuando el usuario deja de escribir

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -10,7 +10,6 @@ import { Country } from '../../interfaces/country';
 })
 export class ByCapitalPageComponent {
   public countries: Country[] = [];
-// 110 Crear propiedad para carga de espinner.
  public isLoading:boolean = false;
 
 
@@ -18,12 +17,10 @@ export class ByCapitalPageComponent {
 
 
   searchByCapital(term:string):void{
-// 111.  Al iniciar este metodo cambiar a true isloading
     this.isLoading = true;
     this.countriesService.searchCapital(term)
     .subscribe(countries => {
       this.countries = countries;
-// 112. Al terminar de hacer la peticion cambiar a false
       this.isLoading = false;
     });
 
--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,5 +1,6 @@
 
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,16 +8,50 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
 
-@Input()
+// 117. Implementar el onInit
+export class SearchBoxComponent implements OnInit {
+  // 116. Crear nueva propiedad privada con un observable subject
+  private debouncer : Subject <string> = new Subject<string>();
+
+
+  @Input()
   public placeholder:string = '';
 
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+// 121. Agregar un emisor de eventos
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+// 118. Establecer una suscripción al debouncer y muestrar (*en consola) los valores emitidos por él
+// 120. Agregarle debounce time con el pipe
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(500)
+    )
+    .subscribe(value =>{
+      // console.log('debouncer value', value);
+      // 122. Llamar al ondebounce y el valor que emite
+      this.onDebounce.emit(value);
+    })
+  }
+
+
   emitValue(value:string):void{
     this.onValue.emit(value);
+  }
+
+
+
+// 115. Crear nuevo metodo llamado
+  onKeyPress( searchTerm:string){
+    // console.log(searchTerm);
+// 119. Llamar al debouncer
+    this.debouncer.next(searchTerm);
+
   }
 }
