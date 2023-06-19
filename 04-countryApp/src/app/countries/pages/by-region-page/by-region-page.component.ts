1b67806ed4283f31a0059492214e79288636a508
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-18 19:26:01 -0600

    CountryApp: Buscar por región optimización con botones

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
+// 130. Hacer un tipo (similar a interface) con los valores permitidos para regiones
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 
 @Component({
   selector: 'app-by-region-page',
@@ -10,16 +13,21 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
-
+  // 129. Hacer un arreglo con las regiones permitidas
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+// 133 . Hacer propiedad publica para que el boton se muestre activo
+  public  selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService){}
 
+// 134. Modificar el termino recibido de string por region en todo el metodo.
+searchByRegion(region: Region):void{
+// 135. asignar el valor de la region al hacer clic al estado active
+  this.selectedRegion = region;
 
-searchByRegion(term:string):void{
-  this.countriesService.searchRegion(term)
+  this.countriesService.searchRegion(region)
   .subscribe(countries => {
     this.countries = countries;
   });
-
 }
 }
