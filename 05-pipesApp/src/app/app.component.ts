6a2890d876201575dabcf6ffbc138c653daf0610
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-22 17:12:25 -0600

    PipesApp: Instalacion de PrimeNG

--- 05-pipesApp/src/app/app.component.ts
+++ 05-pipesApp/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+// 7. Para configurar animaciones "Ripple" importarlas aqui import { PrimeNGConfig } from 'primeng/api';
+
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public title = 'chRisTiAN roberTo';
+
+// 8. De la documentacion copiar el constructor e implementar el oninit
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+    }
+
 }
--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// 9. Importar el modulo de botones y agregarlo a los imports
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
