19440221e14ef72a23cdea20b66127f54728bac8
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-23 10:08:28 -0600

    PipesApp: Cambiar idioma global y manualmente

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// 12. Importar el sharedmodule
 import { SharedModule } from './shared/shared.module';
-// 18. Instalar e Importar el modulo de animaciones
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Configuraciones locales de la app
+import localeEsMX  from '@angular/common/locales/es-MX'
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEsMX)
 
 @NgModule({
   declarations: [
@@ -17,7 +20,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-MX'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
