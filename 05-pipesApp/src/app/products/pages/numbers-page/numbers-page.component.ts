c66a506f7e2c97affe53fee3ca462ea118271852
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-23 11:01:37 -0600

    PipesApp: Decimal, Currency y Percent Pipes

--- 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./numbers-page.component.css']
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 2422.2243
+  public percent: number = 0.5435
 }
