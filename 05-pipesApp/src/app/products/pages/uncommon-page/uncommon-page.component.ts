f91559527e7702c5ca3a1f6637cff5e30ce5ced5
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-23 19:02:55 -0600

    PipeApp: Async Pipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -38,4 +39,14 @@ export class UncommonPageComponent {
     address: 'Merida'
   }
 
+  //Async Pipe
+  public myObservableTime = interval(2000).pipe(
+    tap(value => console.log('tap:', value))
+    );
+
+  public promiseValue: Promise<string> = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('Tenemos data en la promesa')
+    },3500)
+  })
 }
