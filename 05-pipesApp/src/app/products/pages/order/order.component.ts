60e06ac92b60232831e091fc36a3e57828c43014
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-25 14:49:44 -0600

    PipesApp: Ordenar tabla por Nombre, Vuela y Color

--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -7,16 +7,13 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase:boolean = false;
+  public orderBy?: keyof Hero;
+
   public heroes: Hero [] = [
     {
       name: 'Superman',
       canFly: true,
       color: Color.blue
-    },
-    {
-      name: 'Batman',
-      canFly: false,
-      color: Color.black
     },
     {
       name: 'Batman',
@@ -42,14 +39,14 @@ export class OrderComponent {
 
 
 
-
   toggleUpperCase():void {
     this.isUpperCase = !this.isUpperCase
-
-
   }
 
 
+  changeOrder(value: keyof Hero):void{
+    this.orderBy = value;
+  }
 
 
 }
