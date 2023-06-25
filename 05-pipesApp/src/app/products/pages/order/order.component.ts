48c14d442a4448b3f5e760a11b94e887b10b9cbb
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-24 23:32:28 -0600

    PipesApp: Argumentos dinámicos a pipes personalizados

--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './order.component.html',
 })
 export class OrderComponent {
+  public isUpperCase:boolean = false;
+
+  toggleUpperCase():void {
+    this.isUpperCase = !this.isUpperCase
+  }
+
+
+
 
 }
