cab8dd4c1f91993c89cd15367602956a81e5d626
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-06-30 00:12:11 -0600

    HeroesApp: Variables de entorno - Angular +15

---
+++ 06-heroesApp/src/environments/environments.prod.ts
@@ -0,0 +1,11 @@
+// 41. Hacer las variables de entorno con liga de produccion. Se coloca un ejemplo
+// 42. Para que estos archivos sean reconocidos por angular  hacer las siguientes configuraciones, Ir al archivo angular.json y en configurations", "production": agregar fileReplacements
+      // "fileReplacements":[
+      //   {
+      //     "replace":"src/environments/environments.ts",
+      //     "with":"src/environments/environments.prod.ts"
+      //   }
+
+export const environments ={
+  baseUrl: 'http://123456.com/api'
+}
---
+++ 06-heroesApp/src/environments/environments.ts
@@ -0,0 +1,5 @@
+// 39. Crear una carpeta para las variables (url) y los dos archivos ts uno para desarrollo y uno para produccion
+// 40. Comenzar con las variables de desarrollo
+export const environments ={
+  baseUrl: 'http://localhost:3000'
+}
