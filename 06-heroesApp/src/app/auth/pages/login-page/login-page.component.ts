a461ab8eb2f3e448d63ca190e12ca39970fe4a5b
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-07-07 13:23:39 -0600

    HeroesApp: Autenticar y mostrar usuario activo

--- 06-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
+++ 06-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginPageComponent {
+
+  //126. Inyectar el servicio autservice y de Router
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ){}
+
+  //125. Crear el metodo onlogin
+  onLogin():void {
+    this.authService.login('token','agarga.ahahah.hdhdhdh')
+    .subscribe(user =>{
+      this.router.navigate(['/']);
+    })
+  }
+
 
 }
--- 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -14,4 +17,24 @@ export class LayoutPageComponent {
     {label: 'Añadir', icon:'add', url:'./new-hero'},
     {label: 'Buscar', icon:'search', url:'./search'}
   ]
+  //127. Iyectar el servicio
+  // 130. Crear propiedad
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ){}
+
+  //129. Crear un metodo para obtener el usuario
+  get user(): User | undefined{
+    return this.authService.currentUser
+  }
+
+  //128. Crear el metodo onLogout
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['auth/login'])
+  }
+
+
+
 }
