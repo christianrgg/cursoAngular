a9f271bf1c87bac5124319a938628d8fa87244ba
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-07-07 23:24:12 -0600

    Heroes App: PublicGuard

--- 06-heroesApp/src/app/auth/guards/public.guard.ts
+++ 06-heroesApp/src/app/auth/guards/public.guard.ts
@@ -7,34 +7,37 @@ import {
   RouterStateSnapshot,
   UrlSegment,
 } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable, tap, map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 
 
+
 const checkAuthStatus = (): boolean | Observable<boolean> => {
+
   const authService: AuthService = inject(AuthService);
   const router: Router = inject(Router);
 
   return authService.checkAuthentication().pipe(
     tap((isAuthenticated) => {
-      if (!isAuthenticated) {
-        router.navigate(['/auth/login']);
+      if (isAuthenticated) {
+        router.navigate(['./']);
       }
-    })
+    }),
+    map(isAuthenticated => !isAuthenticated)
   );
 
 };
 
 
-export const canActivateGuard: CanActivateFn = (
+export const canActivatePublic: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
   return checkAuthStatus();
 };
 
-export const canMatchGuard: CanMatchFn = (
+export const canMatchPublic: CanMatchFn = (
   route: Route,
   segments: UrlSegment[]
 ) => {
